bbeeffa1914c912580aa9dd0d20929fbb1fdf594
Veikko Aho <vidfinnx@yahoo.com>
2021-09-06 00:48:38 -0700

    snake game and iframe added responsive

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <Archeader/>
        <Cabinet/>
-       <Touchcontrols/>
+       {/* <Touchcontrols/> */}
   </div>
   );
 }
---
+++ src/Snakegame.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Snakegame = () => {
+  return (
+    <div className="snakegame">
+      <iframe scrolling = "no" title="games" src="http://vsnake.herokuapp.com"></iframe>
+    </div>
+  );
+}
+
+export default Snakegame;
