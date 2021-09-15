58109f72c59b66e39ceff8a98fbb167fd521c8e8
Veikko Aho <Vidfinnx@yahoo.com>
2021-09-14 23:32:10 -0700

    modules delete

--- client/src/components/Scoreboard/Scoreboard.js
+++ client/src/components/Scoreboard/Scoreboard.js
@@ -2,23 +2,17 @@ import React from 'react'
 import './scoreboard.css'
 import { useQuery } from "@apollo/client";
 import { QUERY_SCORES } from '../../utils/queries';
-// import { useContext } from "react";
-// import { UserContext } from "../../UserContext";
+
 
 const Scoreboard = () => {
-    const   {loading, data}  = useQuery(QUERY_SCORES);
+    const   {loading,error, data}  = useQuery(QUERY_SCORES);
     // const {loggedInUser, setLoggedIn} = useContext(UserContext)
   
     if (loading) return <p>Loading...</p>;
+    if (error) return `Error! ${error}`;
     const leaders = data.users.map((users) => users);
     // console.log(leaders)
 
-    const scores = data.users.map((scores) => scores.score);
-    // console.log(scores)
-    const names = data.users.map((names) => names.username);
-    // console.log(names)
-    
-  
 
     return (
         <div className="scoreboard">
