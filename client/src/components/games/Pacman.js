2e2cdb1acabcf6c0c128fcf21bb07fcfbbe5337e
Veikko Aho <Vidfinnx@yahoo.com>
2021-09-17 01:06:35 -0700

    RE-ENABLED HASHING AGAIN

--- client/src/components/games/Pacman.js
+++ client/src/components/games/Pacman.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 
+// function checkFocus() {
+//     if(document.activeElement == document.getElementsByTagName("iframe")[0]) {
+//       console.log('iframe has focus');
+//     } else {
+//       console.log('iframe not focused');
+//     }
+//   }
 
+  // window.setInterval(checkFocus, 1000); 
+
+  
+  // function IFocus() {
+  //   document.getElementsByTagName("iframe")[0].focus();
+
+  //  };
+  //  window.setInterval(IFocus, 300);
 
 
  
